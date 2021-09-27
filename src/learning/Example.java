40e8a2ba04316ab5afe1b197f1171ed9205fd277
Hritvik-Mohan <hritvik5005@gmail.com>
2021-09-27 11:31:03 +0530

    Made changes in Example.java

--- src/learning/Example.java
+++ src/learning/Example.java
@@ -1,6 +1,6 @@
 package src.learning;
 class Example {
     public static void main (String args[]) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Mohan!");
     }
 }
