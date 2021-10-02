94cc6705d60a355756df7b03b1288120e4eecb6f
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2021-10-02 20:59:38 +0530

    Funtions in java

---
+++ src/learning/Functions.java
@@ -0,0 +1,10 @@
+package src.learning;
+
+public class Functions {
+    public static void main(String[] args) {
+        System.out.println(sum(10,20));
+    }
+    static int sum(int a, int b) {
+        return a+b;
+    }
+}
