5658d48e209f408b914da487740e2a953fcb4e08
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2021-10-06 22:21:54 +0530

    Inheritence1.java program to demonstrate Inheritence in Java

---
+++ src/college/Inheritence1.java
@@ -0,0 +1,25 @@
+package src.college;
+import java.io.*;
+
+class A {
+    void sum(int a, int b) {
+        System.out.println("Sum: "+(a+b));
+    }
+}
+
+class B extends A{
+    public static void main(String[] args) {
+        Console c = System.console();
+        int a = Integer.parseInt(c.readLine("Enter num1: "));
+        int b = Integer.parseInt(c.readLine("Enter num2: "));
+
+        B b1 = new B();
+        b1.sum(a, b);
+
+        sub(a, b);
+    }
+
+    static void sub(int a, int b){
+        System.out.println("Difference: "+(a-b));
+    }
+}
