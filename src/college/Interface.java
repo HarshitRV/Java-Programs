0a935e4fdb223b46f19c244edc3448b89dd32e01
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2021-10-15 09:49:45 +0530

    Formatted code.

--- src/college/Interface.java
+++ src/college/Interface.java
@@ -1,37 +1,39 @@
 package src.college;
+
 import java.io.*;
-interface InterfaceShape{
-     void area(int n1, int n2);
+
+interface InterfaceShape {
+    void area(int n1, int n2);
 }
 
-class Square1 implements InterfaceShape{   
-public void area(int n1, int n2){
-    System.out.println("Area of square is "+(n1*n2));
-}
-public static void main(String[] args) {
-    Console c = System.console();
-    int n = Integer.parseInt(c.readLine("enter the number:"));
-   
-    Square1 s1 = new Square1();
-    
-    s1.area(n,n);
-    
-}
-}
-
-class Rectangle1 implements InterfaceShape{
-    public void area(int n1, int n2){
-
-        System.out.println("Area of reactangle is "+(n1*n2));
+class Square1 implements InterfaceShape {
+    public void area(int n1, int n2) {
+        System.out.println("Area of square is " + (n1 * n2));
     }
+
+    public static void main(String[] args) {
+        Console c = System.console();
+        int n = Integer.parseInt(c.readLine("enter the number:"));
+
+        Square1 s1 = new Square1();
+
+        s1.area(n, n);
+
+    }
+}
+
+class Rectangle1 implements InterfaceShape {
+    public void area(int n1, int n2) {
+
+        System.out.println("Area of reactangle is " + (n1 * n2));
+    }
+
     public static void main(String[] args) {
         Console c = System.console();
         int n1 = Integer.parseInt(c.readLine("enter the length:"));
         int n2 = Integer.parseInt(c.readLine("enter the breath:"));
-        Rectangle1 r1=new Rectangle1();
-        r1.area(n1,n2);
+        Rectangle1 r1 = new Rectangle1();
+        r1.area(n1, n2);
     }
-    
+
 }
-    
-    
