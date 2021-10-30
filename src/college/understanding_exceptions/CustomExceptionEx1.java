08b72106079cbd82dbdf766af44637f51dbf5408
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2021-10-30 22:15:30 +0530

    Added programs on custom exceptions in College Folder

---
+++ src/college/understanding_exceptions/CustomExceptionEx1.java
@@ -0,0 +1,33 @@
+/*
+    Sample code to demonstrate the use of custom exceptions.
+    This code is not intended to be run.
+
+    Points to remember:
+    1. Exception class is a checked exception.
+    2. Custom exceptions are not checked by the compiler.
+    3. Checked exceptions are cumpulsory with the try-catch block.
+    4. Unchecked exceptions are not cumpulsoary with the try-catch block.
+
+*/
+package src.college.understanding_exceptions;
+
+class CustomException extends Exception {
+    CustomException(String message) {
+        super(message);
+    }
+}
+    
+class UseCustomException {
+    public static void main(String[] args) {
+        try {
+            int a = 1;
+            if (a < 10) {
+                throw new CustomException("a is less than 10");
+            }
+            System.out.println("Executed");
+        } catch (CustomException e) {
+            // .getMessgae() returns the message of the exception
+            System.out.println(e.getMessage());
+        }   
+    }
+}
