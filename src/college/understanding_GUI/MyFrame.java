5e50bc4bc6f92e8ede9d492db037356f1adcaf72
KUSH <kushagrajaiswal00@gmail.com>
2021-11-13 23:28:30 +0530

    Added GUI Files

---
+++ src/college/understanding_GUI/MyFrame.java
@@ -0,0 +1,12 @@
+package src.college.understanding_GUI;
+import java.awt.*;
+public class MyFrame {
+    public static void main(String[] args) {
+        Frame f1 = new Frame("Year 2022");
+        f1.setBackground(Color.green);;
+        f1.setSize(300,300); 
+        f1.setVisible(true);
+
+    }
+    
+}
