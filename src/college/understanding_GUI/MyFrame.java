4d311ffcce01b1fb7d4321ce3c6f1f7164733ab9
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2021-11-14 16:57:06 +0530

    Delted the java program on buttons and combined the MyFrame and MyFrame2 in one file MyFrame

--- src/college/understanding_GUI/MyFrame.java
+++ src/college/understanding_GUI/MyFrame.java
@@ -1,12 +1,30 @@
+/*
+    Sample code to show on how to create
+    window using java.awt.*  
+*/
 package src.college.understanding_GUI;
 import java.awt.*;
-public class MyFrame {
+class MyFrame {
     public static void main(String[] args) {
-        Frame f1 = new Frame("Year 2022");
-        f1.setBackground(Color.green);;
-        f1.setSize(300,300); 
-        f1.setVisible(true);
+
+        // CREATING FRAME WINDOW DIRECTLY USING THE FRAME CLASS
+        Frame f2 = new Frame("");
+        f2.setBackground(Color.green);;
+        f2.setSize(300,300); 
+        f2.setVisible(true);
 
     }
-    
+}
+// CREATING OWN FRAME CLASS BY INHERITING THE FRAME CLASS
+class MyFrame_2 extends Frame {
+    MyFrame_2(String str){
+        super(str);
+    }
+    public static void main(String[] args) {
+        // CREATING FRAME WINDOW USING MY OWN FRAME CLASS
+        MyFrame_2 f1 = new MyFrame_2("Window using Frame_2");
+        f1.setBackground(Color.yellow);
+        f1.setSize(300,300);
+        f1.setVisible(true);
+    }
 }
