f764250b71e98f6001b3f84f4875fb600394e5f8
KUSH <kushagrajaiswal00@gmail.com>
2021-11-14 18:26:46 +0530

    Added 2 programs

--- src/college/understanding_GUI/MyFrame3.java
+++ src/college/understanding_GUI/MyFrame3.java
@@ -1,18 +1,17 @@
 package src.college.understanding_GUI;
 import java.awt.*;
 public class MyFrame3 extends Frame {
-    Button b1;
-
-    MyFrame3(String str);
-    super();
-    b1 = new Button("Click Me");
-    add(b1);
-    set Size (200,300);
-    set Visible (true);
-
+    public static void main(String[] args) {
+        MyFrame3 f1 = new MyFrame3("Button Example");
+       Button b1 = new Button("Click Me");
+        f1.add(b1,BorderLayout.CENTER);
+        f1.setSize (200,300);
+        f1.setVisible (true);
+    
+    }
+    MyFrame3(String str){
+    super(str);
+    }
 }
-public static void main(String[] args) {
-    MyFrame3 f1 = new MyFrame3("Button Example");
 
-}
-}
+
