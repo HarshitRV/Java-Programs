326cd81a6559346a63d96a5234f892cdcf96310f
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2021-11-27 14:36:36 +0530

    custom layout in java awt

---
+++ src/college/understanding_GUI/CustomLayout.java
@@ -0,0 +1,39 @@
+package src.college.understanding_GUI;
+
+import java.awt.*;
+
+public class CustomLayout extends Frame {
+    Label l1, l2;
+    TextField tf1, tf2;
+    Button b1;
+
+    CustomLayout (){
+        l1=new Label("Enter num1: ");
+        l2=new Label("Enter num2: ");
+        tf1=new TextField(12);
+        tf2=new TextField(12);
+        b1=new Button("Add");
+
+        setLayout(null);
+        setSize(300, 200);
+        setVisible(true);
+
+        l1.setBounds(30, 30, 80, 30);
+        tf1.setBounds(130,35, 100, 30 );
+
+        l2.setBounds(30, 70, 80, 30);
+        tf2.setBounds(130,75, 100, 30);
+
+        b1.setBounds(115, 120, 70, 50);
+
+        add(l1);
+        add(tf1);
+        add(l2);
+        add(tf2);
+        add(b1);
+    }
+
+    public static void main(String[] args) {
+        CustomLayout cl = new CustomLayout();
+    }
+}
---
+++ src/college/understanding_GUI/FlowLayoutEx1.java
@@ -0,0 +1,34 @@
+// Program #1 to demonstrate the flow layout in java awt.
+
+package src.college.understanding_GUI;
+
+import java.awt.*;
+class FlowLayoutEx1 extends Frame{
+    Label l1, l2;
+    TextField tf1, tf2;
+    Button b1;
+    FlowLayoutEx1(){
+        // declaring the elements
+        l1 = new Label("Enter num1: ");
+        l2 = new Label("Enter num2: ");
+        tf1 = new TextField(12);
+        tf2 = new TextField(12);
+        b1 = new Button("Add");
+
+        // adding the elements to the layout
+        add(l1);
+        add(tf1);
+        add(l2);
+        add(tf2);
+        add(b1);
+
+        // setting up the layout
+        setLayout(new FlowLayout());
+        setSize(200, 120);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        FlowLayoutEx1 f1 = new FlowLayoutEx1();
+    }
+}
