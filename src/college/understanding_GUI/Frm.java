5bdaa0e150f98a81b28c123c009410c706c7d1b1
Hritvik-Mohan <hritvik5005@gmail.com>
2021-12-03 20:12:37 +0530

    Added new programs in understanding_GUI

---
+++ src/college/understanding_GUI/Frm.java
@@ -0,0 +1,37 @@
+package src.college.understanding_GUI;
+
+import java.awt.*;
+import java.awt.event.*;
+
+class Frm extends Frame implements ActionListener{
+    Label l;
+    TextField tf;
+    Button b;
+
+    Frm(String s){
+
+        super(s);
+
+        l = new Label("Enter Name: ");
+        tf = new TextField(10);
+        b = new Button("Show");
+
+        add(l); add(tf); add(b);
+
+        b.addActionListener(this);
+
+        setLayout(new FlowLayout());
+        setSize(200,300);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        String s = tf.getText();
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        Frm f = new Frm("Action Listner");
+    }
+
+}
