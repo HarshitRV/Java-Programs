30e2552309987042bd7c5f4a4a37ccb52a905407
Sudhanshu kumar <mauryasudhanshu930@gmail.com>
2022-08-04 20:25:16 +0530

    changes made as per request

--- src/challenges/sudhanshu/EvenOdd.java
+++ src/challenges/sudhanshu/EvenOdd.java
@@ -1,23 +1,17 @@
 package src.challenges.sudhanshu;
+
 import java.io.*;
 
-
-
 public class EvenOdd {
-    public static void main(String args[]){
-        // System.out.println("hello");
+    public static void main(String args[]) {
+
         Console c = System.console();
         int a = Integer.parseInt(c.readLine("Enter a no."));
-        // int  b = Integer.parseInt(c.readLine("Enter a no."))
-        if((a%2) == 0){
-        System.out.println("is even");
-        
-        }else{
-        System.out.println("Odd");
-
+        if ((a % 2) == 0) {
+            System.out.println("is even");
+        } else {
+            System.out.println("Odd");
         }
 
-
-        
-        }
     }
+}
